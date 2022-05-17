09407b7f1de5a6fd2804ed86ae8094b9cfcd1a42
Ethan SAFAR <esafar@student.42.fr>
2022-05-17 18:51:33 +0200

    laser seems ok

--- main.c
+++ main.c
@@ -25,27 +25,43 @@
 #define P3 3*PI/2
 
 
-#define mapWidth 8
-#define mapHeight 8
+#define mapWidth 24
+#define mapHeight 24
 #define mapS 64
-#define screenWidth 640
-#define screenHeight 480
+#define screenWidth 1920/2
+#define screenHeight 1080/2
 
-#define w 60
-#define h 60
+#define w 18
+#define h 18
 
 float px, py, pdx, pdy, pa;
 
-int worldMap[]=
+int worldMap[mapWidth][mapHeight]=
 {
-  1,1,1,1,1,1,1,1,
-  1,0,1,0,0,0,0,1,
-  1,0,1,0,0,0,0,1,
-  1,0,1,0,0,0,0,1,
-  1,0,0,0,0,0,0,1,
-  1,0,0,0,0,1,0,1,
-  1,0,0,0,0,0,0,1,
-  1,1,1,1,1,1,1,1,
+  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,1,1,1,1,1,0,0,0,0,1,0,1,0,1,0,0,0,1},
+  {1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1,0,0,0,1,0,0,0,1},
+  {1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,1,1,0,1,1,0,0,0,0,1,0,1,0,1,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,1,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,1,0,0,0,0,5,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,1,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,1,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 };
 
 
@@ -62,7 +78,7 @@ int draw_line(t_test *test, int beginX, int beginY, int endX, int endY, int colo
 	double deltaX = endX - beginX; // 10
 	double deltaY = endY - beginY; // 0
 
-	int pixels = sqrt((deltaX * deltaX) + (deltaY * deltaY));
+	int pixels = sqrt(((deltaX * deltaX) + (deltaY * deltaY)));
 
 	deltaX /= pixels; // 1
 	deltaY /= pixels; // 0
@@ -113,176 +129,201 @@ void	drawBackground(t_test *test)
 		x = 0;
 		while (x < 1024)
 		{
-			my_mlx_pixel_put(&test->data, x, y, 0x808080);
+			my_mlx_pixel_put(&test->data, x, y, 0x000000);
 			x++;
 		}
 		y++;
+	}
+}
+
+void	drawCube(t_test *test, int startx,  int starty, int sizex, int sizey, int color)
+{
+	int i, j;
+
+	i = 1;
+	while (i < sizex)
+	{
+		j = 1;
+		while (j < sizey)
+		{
+			my_mlx_pixel_put(&test->data, startx + i, starty + j, color);
+			j++;
+		}
+		i++;
 	}
 }
 
 void	drawMap2D(t_test *test)
 {
-	int x, y, xo, yo;
 	int i, j;
+	int x, y;
 
-	y = 0;
-	while (y < mapHeight)
+	i = 0;
+	while (i < mapHeight)
 	{
-		x = 0;
-		while (x < mapWidth)
+		j = 0;
+		while (j < mapWidth)
 		{
-			if (worldMap[y * mapWidth + x] == 1)
+			if (worldMap[i][j] == 1)
 			{
-				// printf("[1] ");
-				i=1;
-				while (i<w)
-				{
-					j = 1;
-					while (j<h)
-					{
-						my_mlx_pixel_put(&test->data, (x * w) + i, (y * h) + j, 0xffffff);
-						j++;
-					}
-					i++;
-					// printf("i=[%d]\n", i);
-				}
+				drawCube(test, j * 16, i * 16, 16, 16, 0xffffff);
 			}
-			else
-			{
-				// printf("[0] ");
-				i=1;
-				while (i<w)
-				{
-					j = 1;
-					while (j<h)
-					{
-						my_mlx_pixel_put(&test->data, (x * w) + i, (y * h) + j, 0x000000);
-						j++;
-					}
-					i++;
-				}
-			}
-			// xo = x * mapS;
-			// yo = y * mapS;
-			x++;
+			j++;
 		}
-		// printf("\n");
-		y++;
+		i++;
 	}
 }
 
 void	drawRays3D(t_test *test)
 {
-	int r, mx, my, mp, dof;
+	int		mx;
+	int		my;
+	int		dof;
 	float rx, ry, ra, xo, yo;
 	ra=pa;
 
-	r = 0;
-	while (r < 1)
+	dof = 0;
+	if (ra < PI)
 	{
-		//-------check horizontal lines-------
-		dof=0;
-		float aTan= -1/tan(ra);
-		if (ra > PI)
-		{
-			ry = (((int)py >> 6) << 6) - 0.0001;
-			rx = (py - ry) * aTan + px;
-			yo = -64;
-			xo = -yo * aTan;
-		}
-		if (ra < PI)
-		{
-			ry = (((int)py >> 6) << 6) + 64;
-			rx = (py - ry) * aTan + px;
-			yo= -64;
-			xo= -yo * aTan;
-		}
-		if (ra == 0 || ra == PI)
-		{
-			rx = px;
-			ry = py;
-			dof = 8;
-		}
-		while (dof < 8)
-		{
-			mx =(int)(rx)>>6;
-			my =(int)(ry)>>6;
-			mp = my * mapWidth + mx;
-			if (mp >0 && mp < mapWidth * mapHeight && worldMap[mp] == 1)
-				dof = 8;// hit wall
-			else //next line
-			{
-				// printf("rx=[%f]et ry=[%f]\n", rx, ry);
-				// fflush(stdout);
-				rx += xo;
-				ry += yo;
-				dof += 1;
-			}
-		}
-		if (rx>0 && ry>0)
-			draw_line(test, px+5,py+5,rx,ry,0x00ff00);
-		
-		//-------check vertical lines-------
-		dof=0;
-		float nTan= -tan(ra);
-		if (ra > P2 && ra < P3)
-		{
-			rx = (((int)px >> 6) << 6) - 0.0001;
-			ry = (px - rx) * nTan + py;
-			xo = -64;
-			yo = -xo * nTan;
-		}
-		if (ra < P2 || ra>P3)
-		{
-			rx = (((int)px >> 6) << 6) + 64;
-			ry = (px - rx) * nTan + py;
-			xo= -64;
-			yo= -xo * nTan;
-		}
-		if (ra == 0 || ra == PI)
-		{
-			rx = px;
-			ry = py;
-			dof = 8;
-		}
-		while (dof < 8)
-		{
-			mx =(int)(rx)>>6;
-			my =(int)(ry)>>6;
-			mp = my * mapWidth + mx;
-			if (mp > 0 && mp < mapWidth * mapHeight && worldMap[mp] == 1)
-				dof = 8;// hit wall
-			else //next line
-			{
-				// printf("rx=[%f]et ry=[%f]\n", rx, ry);
-				fflush(stdout);
-				rx += xo;
-				ry += yo;
-				dof += 1;
-			}
-		}
-		if (rx>0 && ry>0)
-			draw_line(test, px+5,py+5,rx,ry,0x00ff00);
-		r++;
+		ry = (((int)py / 16) * 16) - 0.0001;
+		rx = (py - ry) * (-1 / tan(ra)) + px;
+		yo = -16;
+		xo = -yo * (-1 / tan(ra));
 	}
+	if (ra > PI)
+	{
+		ry = (((int)py / 16) * 16) + 16;
+		rx = (py - ry) * (-1 / tan(ra)) + px;
+		yo = 16;
+		xo = -yo * (-1 / tan(ra));
+	}
+	if (!ra || ra == PI)
+	{
+		rx = px;
+		ry = py;
+		dof = mapHeight;
+	}
+	while (dof < mapHeight)
+	{
+		mx = (int)rx / 16;
+		my = (int)ry / 16;
+		if ((my >= 0 && mx >= 0 && my < mapHeight && mx < mapWidth)
+			&& (worldMap[my][mx] == 1))
+			// return (ft_dist(px, py, rx, ry));
+			break ;
+		else if (my < 0 || my < 0 || my > mapHeight || mx > mapWidth)
+			dof = mapHeight;
+		else
+		{
+			rx += xo;
+			ry += yo;
+			dof = dof + 1;
+		}
+	}
+	if (rx > 0 && ry > 0)
+		draw_line(test, px+5,py+5,rx,ry,0x00ff00);
 }
+
+// void	drawRays3D(t_test *test)
+// {
+// 	int r, mx, my, mp, dof;
+// 	float rx, ry, ra, xo, yo;
+// 	ra=pa;
+
+// 	r = 0;
+// 	while (r < 1)
+// 	{
+// 		//-------check horizontal lines-------
+// 		dof=0;
+// 		float aTan= -1/tan(ra);
+// 		if (ra > PI)
+// 		{
+// 			ry = (((int)py >> 6) << 6) - 0.0001;
+// 			rx = (py - ry) * aTan + px;
+// 			yo = -64;
+// 			xo = -yo * aTan;
+// 		}
+// 		if (ra < PI)
+// 		{
+// 			ry = (((int)py >> 6) << 6) + 64;
+// 			rx = (py - ry) * aTan + px;
+// 			yo= 64;
+// 			xo= -yo * aTan;
+// 		}
+// 		if (ra == 0 || ra == PI)
+// 		{
+// 			rx = px;
+// 			ry = py;
+// 			dof = 8;
+// 		}
+// 		while (dof < 8)
+// 		{
+// 			mx =(int)(rx)>>6;
+// 			my =(int)(ry)>>6;
+// 			if (my >= 0 && mx >= 0 && my < mapHeight && mx < mapWidth && worldMap[my][mx] == 1)
+// 				break ;
+// 			else if (my < 0 || mx < 0 || my > mapHeight || mx > mapWidth)
+// 				dof = mapHeight;
+// 			else //next line
+// 			{
+// 				rx += xo;
+// 				ry += yo;
+// 				dof += 1;
+// 			}
+// 		}
+// 		if (rx > 0 && ry > 0)
+// 			draw_line(test, px,py,rx,ry,0x00ff00);
+		
+		
+// 		//-------check vertical lines-------
+// 		// dof=0;
+// 		// float nTan= -tan(ra);
+// 		// if (ra > P2 && ra < P3)
+// 		// {
+// 		// 	rx = (((int)px >> 6) << 6) - 0.0001;
+// 		// 	ry = (px - rx) * nTan + py;
+// 		// 	xo = -64;
+// 		// 	yo = -xo * nTan;
+// 		// }
+// 		// if (ra < P2 || ra>P3)
+// 		// {
+// 		// 	rx = (((int)px >> 6) << 6) + 64;
+// 		// 	ry = (px - rx) * nTan + py;
+// 		// 	xo= -64;
+// 		// 	yo= -xo * nTan;
+// 		// }
+// 		// if (ra == 0 || ra == PI)
+// 		// {
+// 		// 	rx = px;
+// 		// 	ry = py;
+// 		// 	dof = 8;
+// 		// }
+// 		// while (dof < 8)
+// 		// {
+// 		// 	mx =(int)(rx)>>6;
+// 		// 	my =(int)(ry)>>6;
+// 		// 	mp = my * mapWidth + mx;
+// 		// 	if (mp > 0 && mp < mapWidth * mapHeight && worldMap[mp] == 1)
+// 		// 		dof = 8;// hit wall
+// 		// 	else //next line
+// 		// 	{
+// 		// 		// printf("rx=[%f]et ry=[%f]\n", rx, ry);
+// 		// 		fflush(stdout);
+// 		// 		rx += xo;
+// 		// 		ry += yo;
+// 		// 		dof += 1;
+// 		// 	}
+// 		// }
+// 		// if (rx>0 && ry>0)
+// 		// 	draw_line(test, px+5,py+5,rx,ry,0x00ff00);
+// 		r++;
+// 	}
+// }
 
 void  drawPlayer(t_test *test)
 {
-	int x, y;
-
-	x = 0;
-	while (x < 10)
-	{
-		y = 0;
-		while (y < 10)
-		{
-			my_mlx_pixel_put(&test->data, px + x, py + y, 0xffff00);
-			y++;
-		}
-		x++;
-	}
-	draw_line(test, px + 5, py + 5, px + 5 + pdx * 5, py + 5 + pdy * 5, 0xf00020);
+	drawCube(test, px, py, 10, 10, 0xffff00);
+	// draw_line(test, px + 5, py + 5, px + 5 + pdx * 5, py + 5 + pdy * 5, 0xf00020);
 }
 
 int	render(t_test *test)
@@ -301,14 +342,14 @@ int	close_win_cross(t_test *test)
 	return (0);
 }
 
-int	handle_keypress(int keysym, t_test *test)
+int	handle_kepyress(int keysym, t_test *test)
 {
 	if (keysym == ESC)
 		exit(0);
 	else if (keysym == W)
 	{
-		px+=pdx;
-		py+=pdy;
+		px-=pdx;
+		py-=pdy;
 	}
 	else if (keysym == A)
 	{
@@ -321,9 +362,9 @@ int	handle_keypress(int keysym, t_test *test)
 		pdy=sin(pa) *5;
 	}
 	else if (keysym == S)
-	{
-		px-=pdx;
-		py-=pdy;
+	{	
+		px+=pdx;
+		py+=pdy;
 	}
 	else if (keysym == D)
 	{
@@ -361,7 +402,7 @@ int	main(int ac, char **av)
 	test.data.img = mlx_new_image(test.mlx, 1024, 512);
 	test.data.addr = mlx_get_data_addr(test.data.img, &test.data.bits_per_pixel, &test.data.line_length, &test.data.endian);
 	// get_all_images_and_addresses(&test);
-	mlx_hook(test.win, 2, 1L << 0, &handle_keypress, &test);
+	mlx_hook(test.win, 2, 1L << 0, &handle_kepyress, &test);
 	mlx_loop_hook(test.mlx, render, &test);
 	mlx_hook(test.win, 17, 1L << 0, &close_win_cross, &test);
 	mlx_loop(test.mlx);
