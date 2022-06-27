b99905e54b955d19b4425fa593071271fd114b4d
Alexandre Chane-Law <narguss@airdealexandre.home>
2022-06-27 23:48:17 +0200

    Corecction unused variable libft

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: esafar <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: achane-l <achane-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 14:47:42 by esafar            #+#    #+#             */
-/*   Updated: 2021/06/04 09:30:12 by esafar           ###   ########.fr       */
+/*   Updated: 2022/06/27 23:45:45 by achane-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,9 @@ char	*ft_strrchr(const char *s, int c)
 {
 	size_t	i;
 	char	*res;
-	char	*str;
 
 	i = 0;
 	res = NULL;
-	str = (char *)s;
 	while (s[i] != '\0')
 	{
 		if (s[i] == (char)c)
